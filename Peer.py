bd02c9c2c77fc4a185581281eeba247df85a2037
Bertine LEMEE <E145413H@U-INF-J-C002-20.iut-nantes.univ-nantes.prive>
2016-02-25 10:39:44 +0100

    Classe Peer

--- Peer.py
+++ Peer.py
@@ -1,3 +1,30 @@
 # Classe Peer
 
 
+class Peer :
+
+	def __init__(self, ip, hash) :
+		""" Constructeur de la classe Peer
+			Prend en paramètres une ip et le hash associé à cette ip """
+		self.ip = ip
+		self.hash = hash
+		self.routing = {} #Dictionnaire qui correspondond à la table de routage
+	
+
+	def getIP(self) :
+		return self.ip
+
+
+	def getHash(self) :
+		return self.hash
+
+
+	def getRoute(self, hash) :
+		return hash, self.routing[hash]
+
+
+	def addRoute(self,hash, hashSucc, ipSucc) :
+		""" Méthode qui permet d'ajouter une route à la table de routage
+			Route = hash:hash du successeur:ip du successeur """
+		self.routing[hash] = (hashSucc, ipSucc)
+
