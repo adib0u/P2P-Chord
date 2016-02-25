d2c46988f92796d8db597a0331a2afc16bd06fad
gugu4-9 <augustin@mrlacour.fr>
2016-02-25 11:36:05 +0100

    ajout du pair : OK + ajout gitignore

--- join_network.py
+++ join_network.py
@@ -1,11 +1,13 @@
 # Script creating a peer and joining a network
+
 import socket as ss
+from Peer import Peer
 
 # Récupération de l'ip de la machine actuel
 ipClient = ss.gethostbyname(ss.gethostname())
-print(ipClient)
+print("> IP client : " + ipClient)
 
-# demande de hash au HashServer
+# Demande de hash au HashServer
 
 print("Saisir ip du serveur de hash")
 ipHashServeur = input()
@@ -14,7 +16,25 @@ sock = ss.socket()
 sock.connect( (ipHashServeur, 8001) )
 sock.send( str.encode(ipClient + "\n") )
 hashClient = sock.recv( 1024 ).decode()
+hashClient = hashClient[:-1]
 
 sock.close()
 
-print(hashClient)
+print("> Hash client : " + hashClient)
+
+# Création pair
+
+peer1 = Peer( ipClient, hashClient)
+
+print("Saisir ip du serveur d'acceuil")
+ipWelcomeServeur = input()
+
+sock = ss.socket()
+sock.connect( (ipWelcomeServeur, 8000) )
+print("yo:" + hashClient + ":" + ipClient + "\n")
+sock.send( str.encode("yo:" + hashClient + ":" + ipClient + "\n") )
+welcomeAnswer = sock.recv(1024).decode()
+sock.close()
+
+print(welcomeAnswer)
+print("> Pair ajouté au réseau")
